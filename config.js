3148b8be1ed8e5a6f4e8ba037dc9cd4c52d74941
sanchitha23 <167330247+sanchitha23@users.noreply.github.com>
2025-04-12 08:20:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const defaultOwner = '923444844060';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '94742817784' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '94762798019' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'bMJBTB6a#JFM7XOETkDZBfWqmzApC_po26uf4a2_T6nD9-elYOc0' ;  // put your session id here
 
 global.mods = []
 global.prems = []
